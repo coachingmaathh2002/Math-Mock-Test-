e3ac2b281f4ab67e564b122d5fa17ea623a99192
coachingmaathh2002 <coachingmaathh2002@gmail.com>
2025-07-10 12:02:51 +0530

    Update Icon.tsx

--- components/common/Icon.tsx
+++ components/common/Icon.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 interface IconProps {
@@ -25,10 +24,10 @@ export const XCircleIcon: React.FC<IconProps> = ({ className }) => (
 
 export const LogoIcon: React.FC<IconProps> = ({ className }) => (
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h3m-6.75 3.75h9.5M10.5 18h3" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" />
     </svg>
 );
+
 
 export const ArrowPathIcon: React.FC<IconProps> = ({ className }) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={className}>
